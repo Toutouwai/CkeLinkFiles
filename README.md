28296af2331425e199a414975fa40d03e7e02310
Toutouwai <rpsallis@gmail.com>
2018-01-11 15:59:42 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# CKEditor Link Files
+
+A module for ProcessWire CMS/CMF. Adds a menu to CKEditor to allow the quick insertion of links to files on the page being edited.
+
+![link-files](https://user-images.githubusercontent.com/1538852/34805734-8142c638-f6e4-11e7-814a-3655b4e5ec76.gif)
+
+## Features
+
+* Hover a menu item to see the "Description" of the corresponding file (if present).
+* Click a menu item to insert a link to the corresponding file at the current cursor position. The filename is used as the link text.
+* If you Alt-click a menu item the file description is used as the link text (with fallback to filename if no description entered).
+* If text is currently selected in the editor then the selected text is used as the link text.
+* Click "* Insert links to all files *" to insert an unordered list of links to all files on the page. Also works with the Alt-click option.
+* Menu is built via AJAX so newly uploaded files are included in the menu without the page needing to be saved. However, descriptions are not available for newly uploaded files until the page is saved.
+
+
+## Installation
+
+[Install](http://modules.processwire.com/install-uninstall/) the CKEditor Link Files module.
+
+For any CKEditor field where you want the "Insert link to file" dropdown menu to appear in the CKEditor toolbar, visit the field settings and add "LinkFilesMenu" to the "CKEditor Toolbar" settings field.
---
+++ plugin/linkfilesmenu/plugin.js
@@ -0,0 +1,124 @@
+/*
+* Link Files Menu plugin
+*
+* @author Robin Sallis: https://github.com/Toutouwai/CkeLinkFiles
+* @version 0.1.0
+*/
+
+CKEDITOR.plugins.add('linkfilesmenu', {
+	icons: 'linkfilesmenu',
+	hidpi: true,
+	lang: ['en'],
+	init: function(editor) {
+		var $orig_element = $(editor.element.$);
+		var cke_id = $orig_element.attr('id');
+		editor.addCommand('showLinkFilesMenu', {
+			exec: function(editor) {
+				var $container = $(editor.element.$).parent('.InputfieldContent');
+				var $button = $container.find('.cke_button__linkfilesmenu');
+				var $menu = $('#link-files-menu');
+				if(!$menu.length) return;
+				$menu.data('cke-id', cke_id);
+				if($menu.is(":hidden")) {
+					$button.addClass('cke_button_on');
+					var button_offset_top = $button.offset().top;
+					var button_offset_left = $button.offset().left;
+					$container.css('position', 'relative');
+					$menu.css({ top: button_offset_top + 27, left: button_offset_left });
+					$menu.show();
+				} else {
+					$menu.hide();
+					$button.removeClass('cke_button_on');
+				}
+			}
+		});
+		editor.ui.addButton('LinkFilesMenu', {
+			label: editor.lang.linkfilesmenu.lfm_button_label,
+			icon: 'linkfilesmenu',
+			command: 'showLinkFilesMenu'
+		});
+	}
+
+});
+
+$(function() {
+
+	// Add menu on DOM ready
+	addFilesMenu(config.CkeLinkFiles.page_id);
+
+	// Add menu after ajax file upload done
+	$(document).on('AjaxUploadDone', function() {
+		addFilesMenu(config.CkeLinkFiles.page_id);
+	});
+
+	// Menu item clicked
+	$('body').on('click', '#link-files-menu li', function(event) {
+		var use_description = !!event.altKey;
+		var $menu = $('#link-files-menu');
+		var cke_id = $menu.data('cke-id');
+		var editor = CKEDITOR.instances[cke_id];
+		var html = '';
+
+		if($(this).hasClass('no-files')) {
+			// No files
+			hideMenu($menu);
+			return;
+		} else if($(this).hasClass('all-files')) {
+			// All files
+			html = '<ul>';
+			$(this).siblings().each(function() {
+				var text;
+				if(use_description && $(this).attr('title').length) {
+					text = $(this).attr('title');
+				} else {
+					text = $(this).text();
+				}
+				html += '<li><a href="' + $(this).data('url') + '">' + text + '</a></li>';
+			});
+			html += '</ul>';
+		} else {
+			// Individual file
+			var text;
+			var selection = editor.getSelection().getSelectedElement() || editor.getSelection().getSelectedText();
+			if(selection) {
+				// Something is selected in the editor
+				var fragment = editor.getSelection().getRanges()[0].extractContents();
+				var element = CKEDITOR.dom.element.createFromHtml('<a href="' + $(this).data('url') + '"></a>');
+				fragment.appendTo(element);
+				editor.insertElement(element);
+				hideMenu($menu);
+				return;
+			} else if(use_description && $(this).attr('title').length) {
+				text = $(this).attr('title');
+			} else {
+				text = $(this).text();
+			}
+			html = '<a href="' + $(this).data('url') + '">' + text + '</a>'
+		}
+		editor.insertHtml(html);
+		hideMenu($menu);
+	});
+
+});
+
+function addFilesMenu(page_id) {
+	$('#link-files-menu').remove();
+	var ajax_url = ProcessWire.config.urls.admin + 'module/edit?name=CkeLinkFiles&pid=' + page_id;
+	var $list = $('<ul></ul>');
+	$.getJSON(ajax_url).done(function(data) {
+		if(data.length) {
+			$.each(data, function(index, value) {
+				$list.append( $('<li data-url="' + value.url + '" title="' + value.description + '">' + value.basename + '</li>') );
+			});
+			$list.append( $('<li class="all-files">' + config.CkeLinkFiles.all_files_text + '</li>') );
+		} else {
+			$list.append( $('<li class="no-files">' + config.CkeLinkFiles.no_files_text + '</li>') );
+		}
+	});
+	$('body').append( $('<div id="link-files-menu"></div>').append($list) );
+}
+
+function hideMenu($menu) {
+	$('.cke_button__linkfilesmenu').removeClass('cke_button_on');
+	$menu.hide();
+}
